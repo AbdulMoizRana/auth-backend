fc419989383494aee0e68341df220a8cc38dad09
abdulmoizrana <abdulmoizrana.ar@gmail.com>
2023-07-05 20:02:02 +0500

    remove: single user check in send otp api

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -185,46 +185,51 @@ exports.sendOtpEmail = async (req, res) => {
     const userfind = await User.findOne({
       email: email,
     });
+    const characters =
+      "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let otp = "";
+    for (let i = 0; i < 5; i++) {
+      otp += characters[Math.floor(Math.random() * characters.length)];
+    }
+    const transport = await nodemailer.createTransport({
+      service: "Gmail",
+      auth: {
+        user: "confirmationformail@gmail.com",
+        pass: "khedrdaffewfsdxc",
+      },
+    });
+    let info = await transport.sendMail({
+      from: "nodemailer", // sender address
+      to: email, // list of receivers
+      subject: "Please confirm your account",
+      text: `your otp is ${otp}`, // plain text body
+      //   html: `<h1>Email Confirmation</h1><h2>Hello ${email}</h2><p>Thank you for subscribing. Please confirm your email by clicking on the following link</p><a href=${WEBSITE_LINK}/${otp}> Click here</a></div>`
+    });
     if (!userfind) {
-      const characters =
-        "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-      let otp = "";
-      for (let i = 0; i < 5; i++) {
-        otp += characters[Math.floor(Math.random() * characters.length)];
-      }
-      const transport = await nodemailer.createTransport({
-        service: "Gmail",
-        auth: {
-          user: "confirmationformail@gmail.com",
-          pass: "khedrdaffewfsdxc",
-        },
-      });
-      let info = await transport.sendMail({
-        from: "nodemailer", // sender address
-        to: email, // list of receivers
-        subject: "Please confirm your account",
-        text: `your otp is ${otp}`, // plain text body
-        //   html: `<h1>Email Confirmation</h1><h2>Hello ${email}</h2><p>Thank you for subscribing. Please confirm your email by clicking on the following link</p><a href=${WEBSITE_LINK}/${otp}> Click here</a></div>`
-      });
-
       let user = new User({
         email,
         otp,
       });
 
       await user.save();
-
-      return res.status(201).json({
-        status: "Success",
-        message: "Email sent successfully",
-        otp: otp,
-      });
     } else {
-      return res.status(400).json({
-        status: "Failed",
-        message: "user already exists",
-      });
+      // return res.status(400).json({
+      //   status: "Failed",
+      //   message: "user already exists",
+      // });
+      // create a query for a movie to update
+      const query = { email: { $regex: email } };
+      // create a new document that will be used to replace the existing document
+      const replacement = {
+        otp: otp,
+      };
+      const result = await userfind.replaceOne(query, replacement);
     }
+    return res.status(201).json({
+      status: "Success",
+      message: "Email sent successfully",
+      otp: otp,
+    });
   } catch (err) {
     return res.status(400).json({
       status: "Fail",
